7b9da6e4388bdb1938c16806e8a1993330445164
525hanlee <525hanlee@gmail.com>
2019-05-11 03:28:02 +0800

    加上註解

--- js/all.js
+++ js/all.js
@@ -1,10 +1,13 @@
 function time() {
+    //取得時間
     var time = new Date();
     var hour = time.getHours();
     var min = time.getMinutes();
     var sec = time.getSeconds();
+    //將時間套用到CSS角度
     document.querySelector('.hour').style.transform = 'rotate(' + (hour*30+min/2-90) + 'deg)';
     document.querySelector('.min').style.transform = 'rotate(' + (min*6+sec/10) + 'deg)';
     document.querySelector('.sec').style.transform = 'rotate(' + (sec*6+180) + 'deg)';
 }
+//設定每秒執行
 setInterval(time, 1000);
